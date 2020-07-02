40145883642977380aa6ea10cb726193d688a308
José Carlos Siqueira <jose.a.siqueira@capgemini.com>
2020-07-02 09:26:05 -0300

    first commit

---
+++ src/main/java/ccom/siqueira76/petapi/PetApiApplication.java
@@ -0,0 +1,13 @@
+package ccom.siqueira76.petapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ccom/siqueira76/petapi/PetApiApplicationTests.java
@@ -0,0 +1,16 @@
+package ccom.siqueira76.petapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
